f7439dc6b6c941da1f78029191aeceff0f90a634
Angela Zhang <angelaz@mit.edu>
2013-03-02 14:35:24 -0500

    tidied up scraping code

--- data/AbercrombieScraper.py
+++ data/AbercrombieScraper.py
@@ -9,30 +9,12 @@ import os
 import errno
 import re
 
+class GenericScraper:
 
-class AbercrombieScraper:
-
-
-    urls = {
-    "tops": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120207&catalogId=10901&langId=-1&categoryId=120207&storeId=10051&topCategoryId=12203",
-    "tees": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120206&catalogId=10901&langId=-1&categoryId=120206&storeId=10051&topCategoryId=12203",
-    "shirts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120205&catalogId=10901&langId=-1&categoryId=120205&storeId=10051&topCategoryId=12203",
-    "polos": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=119705&catalogId=10901&langId=-1&categoryId=119705&storeId=10051&topCategoryId=12203",
-    "hoodies": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12843&catalogId=10901&langId=-1&categoryId=12843&storeId=10051&topCategoryId=12203",
-    "sweathers": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12282&catalogId=10901&langId=-1&categoryId=12282&storeId=10051&topCategoryId=12203",
-    "outerwear": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12252&catalogId=10901&langId=-1&categoryId=12252&storeId=10051&topCategoryId=12203",
-    "shorts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12262&catalogId=10901&langId=-1&categoryId=12262&storeId=10051&topCategoryId=12203",
-    "jeans": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12261&catalogId=10901&langId=-1&categoryId=12261&storeId=10051&topCategoryId=12203",
-    "sweatpants": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12272&catalogId=10901&langId=-1&categoryId=12272&storeId=10051&topCategoryId=12203",
-    "pants": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12267&catalogId=10901&langId=-1&categoryId=12267&storeId=10051&topCategoryId=12203",
-    "yoga": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=73463&catalogId=10901&langId=-1&categoryId=73463&storeId=10051&topCategoryId=12203",
-    "skirts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13058&catalogId=10901&langId=-1&categoryId=13058&storeId=10051&topCategoryId=12203",
-    "dresses": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12265&catalogId=10901&langId=-1&categoryId=12265&storeId=10051&topCategoryId=12203",
-    "flipflops": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13056&catalogId=10901&langId=-1&categoryId=13056&storeId=10051&topCategoryId=12203",
-    "accessories":"http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13060&catalogId=10901&langId=-1&categoryId=13060&storeId=10051&topCategoryId=12203"
-    }
+    urls = {}
     
-    store = "Abercrombie"
+    def __init__(self, urls):
+        self.urls = urls
 
     def scrape(self):
         for category in self.urls.keys():
@@ -52,6 +34,34 @@ class AbercrombieScraper:
             parsed = BeautifulSoup(fd)
             self.processItem(parsed, item, category)
 
+    # subclasses must define
+    # takes in page parsed by beautiful soup and produces 
+    # a list of item (url + possibly some metadata), i.e.
+    # produces a list of tuples. Assume that the first entry
+    # in list is the url
+    def findItems(self, parsed):
+        # items = []
+        # find item urls and metadata
+        # return items
+        raise "Method not implemented"
+
+    def processItem(self, parsed, item):
+        # get image, save
+        # get title
+        # get price
+        # get color
+        # get metadata
+        # create item in database and save #cannot do right now -AZ
+        raise "Method not implemented"
+
+
+class AbercrombieScraper(GenericScraper):
+
+    store = ""
+    
+    def __init__(self, urls, store):
+        GenericScraper.__init__(self, urls)
+        self.store = "Abercrombie"
 
     # takes in page parsed by beautiful soup and produces 
     # a list of item (url + possibly some metadata), i.e.
@@ -92,5 +102,27 @@ class AbercrombieScraper:
         # create item in database and save #cannot do right now -AZ
 
 
-ab = AbercrombieScraper()
+
+urls = {
+"tops": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120207&catalogId=10901&langId=-1&categoryId=120207&storeId=10051&topCategoryId=12203",
+"tees": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120206&catalogId=10901&langId=-1&categoryId=120206&storeId=10051&topCategoryId=12203",
+"shirts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=120205&catalogId=10901&langId=-1&categoryId=120205&storeId=10051&topCategoryId=12203",
+"polos": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=119705&catalogId=10901&langId=-1&categoryId=119705&storeId=10051&topCategoryId=12203",
+"hoodies": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12843&catalogId=10901&langId=-1&categoryId=12843&storeId=10051&topCategoryId=12203",
+"sweathers": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12282&catalogId=10901&langId=-1&categoryId=12282&storeId=10051&topCategoryId=12203",
+"outerwear": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12252&catalogId=10901&langId=-1&categoryId=12252&storeId=10051&topCategoryId=12203",
+"shorts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12262&catalogId=10901&langId=-1&categoryId=12262&storeId=10051&topCategoryId=12203",
+"jeans": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12261&catalogId=10901&langId=-1&categoryId=12261&storeId=10051&topCategoryId=12203",
+"sweatpants": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12272&catalogId=10901&langId=-1&categoryId=12272&storeId=10051&topCategoryId=12203",
+"pants": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12267&catalogId=10901&langId=-1&categoryId=12267&storeId=10051&topCategoryId=12203",
+"yoga": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=73463&catalogId=10901&langId=-1&categoryId=73463&storeId=10051&topCategoryId=12203",
+"skirts": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13058&catalogId=10901&langId=-1&categoryId=13058&storeId=10051&topCategoryId=12203",
+"dresses": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=12265&catalogId=10901&langId=-1&categoryId=12265&storeId=10051&topCategoryId=12203",
+"flipflops": "http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13056&catalogId=10901&langId=-1&categoryId=13056&storeId=10051&topCategoryId=12203",
+"accessories":"http://www.abercrombie.com/webapp/wcs/stores/servlet/CategoryDisplay?parentCategoryId=13060&catalogId=10901&langId=-1&categoryId=13060&storeId=10051&topCategoryId=12203"
+}
+
+store = "Abercrombie"
+
+ab = AbercrombieScraper(urls, store)
 ab.scrape()
--- data/GenericScraper.py
+++ data/GenericScraper.py
@@ -7,45 +7,45 @@ import urllib
 import urllib2
 import os
 import errno
+import re
 
 class GenericScraper:
-    # subclasses must define
-    urls = []
-    type_mapping = {}
-    high_level_category_mapping = {}
 
-    store = "Unknown"
+    urls = {}
+    
+    def __init__(self, urls):
+        self.urls = urls
 
-    def scrape:
-        for url in urls:
-            scrapeUrl(url)
+    def scrape(self):
+        for category in self.urls.keys():
+            self.scrapeCategory(category)
 
-    def scrapeUrl(url):
+    def scrapeCategory(self, category):
         # read page using beautiful soup
-        fd = urllib2.urlopen(url)
+        fd = urllib2.urlopen(self.urls[category])
         parsed = BeautifulSoup(fd)
 
         # isolate items
-        items = findItems(parsed)
+        items = self.findItems(parsed)
 
+        # process Items
         for item in items:
-            # processItem
-            fd = urllib2.urlopen(item[0])
-            parsed = BeautifulSoup(parsed)
-            processItem(parsed, item)
+            fd = urllib2.urlopen(item)
+            parsed = BeautifulSoup(fd)
+            self.processItem(parsed, item, category)
 
     # subclasses must define
     # takes in page parsed by beautiful soup and produces 
     # a list of item (url + possibly some metadata), i.e.
     # produces a list of tuples. Assume that the first entry
     # in list is the url
-    def findItems(parsed):
+    def findItems(self, parsed):
         # items = []
         # find item urls and metadata
         # return items
         raise "Method not implemented"
 
-    def processItem(parsed, item):
+    def processItem(self, parsed, item):
         # get image, save
         # get title
         # get price
